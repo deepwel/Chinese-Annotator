c3bff823303e5199f6e9e252ce19cbe53efd9dad
andrew peiken <616651082@qq.com>
2017-11-22 14:45:44 +0800

    fix bugs

--- chi_annotator/task_center/tokenizers/char_tokenizer.py
+++ chi_annotator/task_center/tokenizers/char_tokenizer.py
@@ -4,13 +4,14 @@ class CharTokenizer(Component):
     provides = ["tokens"]
 
     def __init__(self):
-        super.__init(Component, self)
+        super(Component, self).__init__()
 
     def train(self, samples):
         pass
 
     def process(self, samples):
         pass
+
 import sys
 sys.path.append("E:\Git\Chinese-Annotator")
 if __name__ == "__main__":
