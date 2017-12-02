32cc338f9572f39648e500e510c654e63fcf75a4
Yunong Pang <pangyunong@comp>
2017-12-02 14:05:24 +0800

    [add] 增加了一个首页，把几个文件都加了进去

--- chi_annotator/webui/assets/src/App.vue
+++ chi_annotator/webui/assets/src/App.vue
@@ -1,23 +1,111 @@
+<!-- It is the main layout as the Starter Page -->
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+<div id="app">
+  <menu-bar/>
+  <div class="content-view">
+	<div id="start-page" class="page" v-if="!loadedFile">
+	  <div class="loading-btn btn">加载数据</div>
+	</div>
   </div>
+  
+</div>
 </template>
 
 <script>
+import MenuBar from './components/MenuBar'
 export default {
-  name: 'app'
+	name: 'app',
+	data: {
+		loadedFile : '',			// 目前已经加载的数据模式		
+	},
+  components: {
+    'menu-bar': MenuBar
+  }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-orient: horizontal;
+	-webkit-box-direction: normal;
+	flex-flow: row nowrap;
+	height: 100%;
+	max-width: 100%;
+	min-height: 100vh;
+	overflow-y: hidden;
+	width: 100%;
+}
+html {
+    font-size: 10px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeSpeed;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+}
+body {
+    height: 100%;
+    width: 100%;
+}
+* {
+  border: 0;
+  box-sizing: border-box;
+  margin: 0;
+  outline: 0;
+  padding: 0; 
+}
+#app > .content-view {
+    background: #f6f6f6;
+    color: var(--color-text);
+    display: -webkit-box;
+    display: flex;
+    -webkit-box-flex: 100%;
+    flex: 100%;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    flex-flow: column nowrap;
+    font-family: var(--font-primary);
+    line-height: 1.5;
+    max-height: 100vh;
+    max-width: 100%;
+    position: relative;
+}
+
+.content-view .page{
+	width:100%;
+	height:100%;
+}
+
+.btn{
+	width: 240px;
+	height:80px;
+	line-height:80px;
+	display:block;
+    font-size: 28px;
+    border-radius: 5px;
+    cursor: pointer;
+    background: #b9b9b9;
+    color: white;
+}
+
+.btn:hover{
+	background: #999999;
+}
+
+.loading-btn{
+    position: absolute;
+    left: 50%;
+    margin-left: -120px;
+    top: 50%;
+    margin-top: -50px;	
 }
 </style>
--- chi_annotator/webui/assets/src/main.js
+++ chi_annotator/webui/assets/src/main.js
@@ -2,14 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
-  components: { App }
+	components: { 'App': App }
 })
