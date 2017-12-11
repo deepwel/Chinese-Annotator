86cb2196ed425d0a5b86a9d03021d4489c9b18cc
JiaLei123 <sfw123817@gmail.com>
2017-12-12 00:12:04 +0800

    add a auto run shell for web ui (#41)
    * support django backend
    
    * implement other function to Django
    
    * remove the Database Setting for Django since it not use model
    
    * add web-ui auto start shell

--- chi_annotator/webui/webuiapis/webuiapis/settings.py
+++ chi_annotator/webui/webuiapis/webuiapis/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'webuiapis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
