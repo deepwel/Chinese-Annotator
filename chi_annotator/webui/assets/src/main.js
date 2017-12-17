5986b40c7bad397eace1a40f000d31e98b3a150a
HarryPorter <yunong@fagougou.com>
2017-12-17 12:54:31 +0800

    [re-structure] 重构了整个文件目录结构

--- chi_annotator/webui/assets/src/main.js
+++ chi_annotator/webui/assets/src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-	components: { 'App': App }
-})
+const app = new Vue({
+	template : "<App/>",
+	components : {App}
+}).$mount('#app');
--- chi_annotator/webui/assets/src/router/index.js
+++ chi_annotator/webui/assets/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import LoadingPage from '@/components/LoadingPage'
+import WorkingSpace from '@/components/WorkingSpace'
+import TextClassification from '@/components/workspaces/TextClassification'
 
-Vue.use(Router)
+const router = new Router({
+	routes : [
+		{path : '/loading', component : LoadingPage},
+		{path : '/working-space' , component : WorkingSpace, children : [
+			{path : 'text-classify', component : TextClassification}
+		]},
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+	]
 })
+export default router
